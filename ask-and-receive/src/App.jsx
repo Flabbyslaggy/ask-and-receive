15e478052cfbe299441b435231a696d6c806cdaa
Tavio Ortiz <tavioortiz@yahoo.com>
2026-05-22 09:27:58 -0400

    fixed background screen jump

--- ask-and-receive/src/App.jsx
+++ ask-and-receive/src/App.jsx
@@ -1033,7 +1033,7 @@ export default function App() {
   return (
     <ThemeProvider activeTheme={activeTheme}>
       <div
-        className={`min-h-screen ${activeTheme.primaryText} bg-cover bg-center bg-fixed`}
+        className={`min-h-screen ${activeTheme.primaryText} bg-cover bg-center`}
         style={{
           backgroundImage: `
     ${activeTheme.backgroundGradient},
